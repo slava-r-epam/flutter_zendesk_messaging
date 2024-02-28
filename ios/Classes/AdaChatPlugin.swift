c71ad773cb6db50a1e975634f36f5dff921078c1
Slava Riabinin <viacheslav.riabinin@headspace.com>
2024-02-28 19:34:55 +0400

    Implement setLanguage, setMetaFields and setSensitiveMetaFields

--- ios/Classes/AdaChatPlugin.swift
+++ ios/Classes/AdaChatPlugin.swift
@@ -3,60 +3,79 @@ import UIKit
 
 public class AdaChatPlugin: NSObject, FlutterPlugin {
   private static var channel: FlutterMethodChannel?
-    
+  
   public static func register(with registrar: FlutterPluginRegistrar) {
     channel = FlutterMethodChannel(name: "ada_chat_flutter", binaryMessenger: registrar.messenger())
     let instance = AdaChatPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel!)
-
+    
     debugPrint("AdaChatPlugin:register: registrar=\(registrar)")
   }
-
+  
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     let method = call.method
     let arguments = call.arguments as? Dictionary<String, Any>
-      let adaChatService = AdaChatService(flutterPlugin: self, channel: AdaChatPlugin.channel!)
-
+    let adaChatService = AdaChatService(flutterPlugin: self, channel: AdaChatPlugin.channel!)
+    
     debugPrint("AdaChatPlugin:handle: call=\(call), method=\(method), arguments=\(String(describing: arguments))")
-
+    
     switch(method){
-      case "show":
-        let handle = arguments!["handle"] as! String
-        let mode = arguments?["mode"] as? String
-        let cluster = arguments?["cluster"] as? String
-        let language = arguments?["language"] as? String
-        let domain = arguments?["domain"] as? String
-        let styles = arguments?["styles"] as? String
-        let greeting = arguments?["greeting"] as? String
-        let metafields = arguments?["metafields"] as? [String : Any]
-        let sensitiveMetafields = arguments?["sensitiveMetafields"] as? [String : Any]
-        let openWebLinksInSafari = arguments?["openWebLinksInSafari"] as? Bool
-        let appScheme = arguments?["appScheme"] as? String
-        let webViewTimeout = arguments?["webViewTimeout"] as? Double
-        let deviceToken = arguments?["deviceToken"] as? String
-        let navigationBarOpaqueBackground = arguments?["navigationBarOpaqueBackground"] as? Bool
-            
-        adaChatService.show(rootViewController: UIApplication.shared.delegate?.window??.rootViewController,
-                            handle: handle,
-                            mode: mode,
-                            cluster: cluster,
-                            language: language,
-                            domain: domain,
-                            styles: styles,
-                            greeting: greeting,
-                            metafields: metafields,
-                            sensitiveMetafields: sensitiveMetafields,
-                            openWebLinksInSafari: openWebLinksInSafari,
-                            appScheme: appScheme,
-                            webViewTimeout: webViewTimeout,
-                            deviceToken: deviceToken,
-                            navigationBarOpaqueBackground: navigationBarOpaqueBackground)
-        break
-      default:
-        print("AdaChatPlugin:handle: command=\(method) not found")
-        break
+    case "show":
+      let handle = arguments!["handle"] as! String
+      let mode = arguments?["mode"] as? String
+      let cluster = arguments?["cluster"] as? String
+      let language = arguments?["language"] as? String
+      let domain = arguments?["domain"] as? String
+      let styles = arguments?["styles"] as? String
+      let greeting = arguments?["greeting"] as? String
+      let metafields = arguments?["metafields"] as? [String : Any]
+      let sensitiveMetafields = arguments?["sensitiveMetafields"] as? [String : Any]
+      let openWebLinksInSafari = arguments?["openWebLinksInSafari"] as? Bool
+      let appScheme = arguments?["appScheme"] as? String
+      let webViewTimeout = arguments?["webViewTimeout"] as? Double
+      let deviceToken = arguments?["deviceToken"] as? String
+      let navigationBarOpaqueBackground = arguments?["navigationBarOpaqueBackground"] as? Bool
+      
+      adaChatService.show(rootViewController: UIApplication.shared.delegate?.window??.rootViewController,
+                          handle: handle,
+                          mode: mode,
+                          cluster: cluster,
+                          language: language,
+                          domain: domain,
+                          styles: styles,
+                          greeting: greeting,
+                          metafields: metafields,
+                          sensitiveMetafields: sensitiveMetafields,
+                          openWebLinksInSafari: openWebLinksInSafari,
+                          appScheme: appScheme,
+                          webViewTimeout: webViewTimeout,
+                          deviceToken: deviceToken,
+                          navigationBarOpaqueBackground: navigationBarOpaqueBackground)
+      break
+      
+    case "setLanguage":
+      let language = arguments?["language"] as! String
+      
+      adaChatService.setLanguage(language: language)
+      break
+      
+    case "setMetaFields":
+      let metafields = arguments?["metafields"] as! [String : Any]
+      
+      adaChatService.setMetaFields(metafields: metafields)
+      break
+      
+    case "setSensitiveMetaFields":
+      let metafields = arguments?["sensitiveMetafields"] as! [String : Any]
+      
+      adaChatService.setSensitiveMetaFields(metafields: metafields)
+      break
+      
+    default:
+      print("AdaChatPlugin:handle: command=\(method) not found")
+      break
     }
-
+    
     result(nil)
   }
 }
--- ios/Classes/AdaChatService.swift
+++ ios/Classes/AdaChatService.swift
@@ -3,183 +3,242 @@ import AdaEmbedFramework
 import Flutter
 
 public class AdaChatService: NSObject {
-    private var adaChatPlugin: AdaChatPlugin? = nil
-    private var channel: FlutterMethodChannel? = nil
-
-    init(flutterPlugin: AdaChatPlugin, channel: FlutterMethodChannel) {
-      debugPrint("AdaChatService:init")
-      self.adaChatPlugin = flutterPlugin
-      self.channel = channel
-    }
-
-    func show(rootViewController: UIViewController?, 
-              handle: String,
-              mode: String? = nil,
-              cluster: String? = nil,
-              language: String? = nil,
-              domain: String? = nil,
-              styles: String? = nil,
-              greeting: String? = nil,
-              metafields: [String: Any]? = nil,
-              sensitiveMetafields: [String:Any]? = nil,
-              openWebLinksInSafari: Bool? = nil,
-              appScheme: String? = nil,
-              webViewTimeout: Double? = nil,
-              deviceToken: String? = nil,
-              navigationBarOpaqueBackground: Bool? = nil) {
-        debugPrint("AdaChatService:show: rootViewController=\(String(describing: rootViewController)), handle=\(String(describing: handle)), mode=\(String(describing: mode)), cluster=\(String(describing: cluster)), language=\(String(describing: language)), domain=\(String(describing: domain)), styles=\(String(describing: styles)), greeting=\(String(describing: greeting)), metafields=\(String(describing: metafields)), sensitiveMetafields=\(String(describing: sensitiveMetafields)), openWebLinksInSafari=\(String(describing: openWebLinksInSafari)), appScheme=\(String(describing: appScheme)), webViewTimeout=\(String(describing: webViewTimeout)), deviceToken=\(String(describing: deviceToken)), navigationBarOpaqueBackground=\(String(describing: navigationBarOpaqueBackground))")
-        
-        channel?.invokeMethod("callback1", arguments: ["key1": "value1"])
-        
-        lazy var adaFramework = AdaWebHost(handle: handle)
-        if cluster != nil {
-            adaFramework.cluster = cluster!
-        }
-        if language != nil {
-            adaFramework.language = language!
-        }
-        if domain != nil {
-            adaFramework.domain = domain!
-        }
-        if styles != nil {
-            adaFramework.styles = styles!
-        }
-        if greeting != nil {
-            adaFramework.greeting = greeting!
-        }
-        if openWebLinksInSafari != nil {
-            adaFramework.openWebLinksInSafari = openWebLinksInSafari!
-        }
-        if appScheme != nil {
-            adaFramework.appScheme = appScheme!
-        }
-        if webViewTimeout != nil {
-            adaFramework.webViewTimeout = webViewTimeout!
-        }
-        if deviceToken != nil {
-            adaFramework.deviceToken = deviceToken!
-        }
-        if navigationBarOpaqueBackground != nil {
-            adaFramework.navigationBarOpaqueBackground = navigationBarOpaqueBackground!
-        }
-        if deviceToken != nil {
-            adaFramework.deviceToken = deviceToken!
-        }
-        if metafields != nil {
-            for pair in metafields! {
-                let val = pair.value
-                if val is String {
-                    adaFramework.setMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! String))
-                } else if val is Bool {
-                    adaFramework.setMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Bool))
-                } else if val is Int {
-                    adaFramework.setMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Int))
-                } else if val is Float {
-                    adaFramework.setMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Float))
-                } else if val is Double {
-                    adaFramework.setMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Double))
-                } else {
-                    print("AdaChatService:show: incorrect metafield value=\(val) for key=\(pair.key)")
-                }
-            }
-        }
-        if sensitiveMetafields != nil {
-            for pair in sensitiveMetafields! {
-                let val = pair.value
-                if val is String {
-                    adaFramework.setSensitiveMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! String))
-                } else if val is Bool {
-                    adaFramework.setSensitiveMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Bool))
-                } else if val is Int {
-                    adaFramework.setSensitiveMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Int))
-                } else if val is Float {
-                    adaFramework.setSensitiveMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Float))
-                } else if val is Double {
-                    adaFramework.setSensitiveMetaFields(builder: MetaFields.Builder().setField(key: pair.key, value: val as! Double))
-                } else {
-                    print("AdaChatService:show: incorrect sensitiveMetafield value=\(val) for key=\(pair.key)")
-                }
-            }
-        }
-        
-        adaFramework.webViewLoadingErrorCallback = { (error) -> Void in
-            self.channel?.invokeMethod("onError", arguments: error)
-        }
-        adaFramework.eventCallbacks = ["*": { (event) -> Void in
-            self.channel?.invokeMethod("onEvent", arguments: event)
-        }]
+  private var adaChatPlugin: AdaChatPlugin? = nil
+  private var channel: FlutterMethodChannel? = nil
+  private var adaFramework: AdaWebHost? = nil
+  
+  init(flutterPlugin: AdaChatPlugin, channel: FlutterMethodChannel) {
+    debugPrint("AdaChatService:init")
+    self.adaChatPlugin = flutterPlugin
+    self.channel = channel
+  }
+  
+  func show(rootViewController: UIViewController?,
+            handle: String,
+            mode: String? = nil,
+            cluster: String? = nil,
+            language: String? = nil,
+            domain: String? = nil,
+            styles: String? = nil,
+            greeting: String? = nil,
+            metafields: [String: Any]? = nil,
+            sensitiveMetafields: [String:Any]? = nil,
+            openWebLinksInSafari: Bool? = nil,
+            appScheme: String? = nil,
+            webViewTimeout: Double? = nil,
+            deviceToken: String? = nil,
+            navigationBarOpaqueBackground: Bool? = nil) {
+    debugPrint("AdaChatService:show: rootViewController=\(String(describing: rootViewController)), handle=\(String(describing: handle)), mode=\(String(describing: mode)), cluster=\(String(describing: cluster)), language=\(String(describing: language)), domain=\(String(describing: domain)), styles=\(String(describing: styles)), greeting=\(String(describing: greeting)), metafields=\(String(describing: metafields)), sensitiveMetafields=\(String(describing: sensitiveMetafields)), openWebLinksInSafari=\(String(describing: openWebLinksInSafari)), appScheme=\(String(describing: appScheme)), webViewTimeout=\(String(describing: webViewTimeout)), deviceToken=\(String(describing: deviceToken)), navigationBarOpaqueBackground=\(String(describing: navigationBarOpaqueBackground))")
     
-//        public var zdChatterAuthCallback: (((@escaping (_ token: String) -> Void)) -> Void)?
-//              zdChatterAuthCallback: (((@escaping (_ token: String) -> Void)) -> Void)? = nil,
-        
-        if mode == "inject" {
-            adaFramework.launchInjectingWebSupport(into: rootViewController!.view)
-        } else if mode == "navigation" {
-            if rootViewController?.navigationController == nil {
-                print("AdaChatService:show: navigationController=nil")
-                return
-            }
-            adaFramework.launchNavWebSupport(from: rootViewController!.navigationController!)
-        } else {
-            if rootViewController == nil {
-                print("AdaChatService:show: rootViewController=nil")
-                return
-            }
-            adaFramework.launchModalWebSupport(from: rootViewController!)
-        }
+    channel?.invokeMethod("callback1", arguments: ["key1": "value1"])
+    
+    let adaFramework = AdaWebHost(handle: handle)
+    if cluster != nil {
+      adaFramework.cluster = cluster!
     }
-
-//     func initialize(channelKey: String) {
-//         debugPrint("ZendeskMessaging:initialize - Channel Key - \(channelKey)\n")
-//         Zendesk.initialize(withChannelKey: channelKey, messagingFactory: DefaultMessagingFactory()) { result in
-//             if case let .failure(error) = result {
-//                 self.zendeskPlugin?.isInitialized = false
-//                 debugPrint("\(self.TAG) - initialize failure - \(error.localizedDescription)\n")
-//                 self.channel?.invokeMethod(ZendeskMessaging.initializeFailure, arguments: ["error": error.localizedDescription])
-//             } else {
-//                 self.zendeskPlugin?.isInitialized = true
-//                 debugPrint("\(self.TAG) - initialize success")
-//                 self.channel?.invokeMethod(ZendeskMessaging.initializeSuccess, arguments: [:])
-//             }
-//         }
-//     }
-
-//     func show(rootViewController: UIViewController?) {
-//         guard let messagingViewController = Zendesk.instance?.messaging?.messagingViewController() else { return }
-//         guard let rootViewController = rootViewController else { return }
-//         rootViewController.present(messagingViewController, animated: true, completion: nil)
-//         debugPrint("\(self.TAG) - show")
-//     }
-//
-//     func loginUser(jwt: String) {
-//         Zendesk.instance?.loginUser(with: jwt) { result in
-//             switch result {
-//             case .success(let user):
-//                 self.channel?.invokeMethod(ZendeskMessaging.loginSuccess, arguments: ["id": user.id, "externalId": user.externalId])
-//                 break;
-//             case .failure(let error):
-//                 debugPrint("\(self.TAG) - login failure - \(error.localizedDescription)\n")
-//                 self.channel?.invokeMethod(ZendeskMessaging.loginFailure, arguments: ["error": nil])
-//                 break;
-//             }
-//         }
-//     }
-//
-//     func logoutUser() {
-//         Zendesk.instance?.logoutUser { result in
-//             switch result {
-//             case .success:
-//                 self.channel?.invokeMethod(ZendeskMessaging.logoutSuccess, arguments: [])
-//                 break;
-//             case .failure(let error):
-//                 debugPrint("\(self.TAG) - logout failure - \(error.localizedDescription)\n")
-//                 self.channel?.invokeMethod(ZendeskMessaging.logoutFailure, arguments: ["error": nil])
-//                 break;
-//             }
-//         }
-//     }
-//     func getUnreadMessageCount() -> Int {
-//         let count = Zendesk.instance?.messaging?.getUnreadMessageCount()
-//         return count ?? 0
-//     }
+    if language != nil {
+      adaFramework.language = language!
+    }
+    if domain != nil {
+      adaFramework.domain = domain!
+    }
+    if styles != nil {
+      adaFramework.styles = styles!
+    }
+    if greeting != nil {
+      adaFramework.greeting = greeting!
+    }
+    if openWebLinksInSafari != nil {
+      adaFramework.openWebLinksInSafari = openWebLinksInSafari!
+    }
+    if appScheme != nil {
+      adaFramework.appScheme = appScheme!
+    }
+    if webViewTimeout != nil {
+      adaFramework.webViewTimeout = webViewTimeout!
+    }
+    if deviceToken != nil {
+      adaFramework.deviceToken = deviceToken!
+    }
+    if navigationBarOpaqueBackground != nil {
+      adaFramework.navigationBarOpaqueBackground = navigationBarOpaqueBackground!
+    }
+    if deviceToken != nil {
+      adaFramework.deviceToken = deviceToken!
+    }
+    if metafields != nil {
+      let metafieldsBuilder = MetaFields.Builder()
+      
+      for pair in metafields! {
+        let val = pair.value
+        if val is String {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! String)
+        } else if val is Bool {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Bool)
+        } else if val is Int {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Int)
+        } else if val is Float {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Float)
+        } else if val is Double {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Double)
+        } else {
+          print("AdaChatService:show: incorrect metafield value=\(val) for key=\(pair.key)")
+        }
+      }
+      
+      adaFramework.setMetaFields(builder: metafieldsBuilder)
+    }
+    if sensitiveMetafields != nil {
+      let metafieldsBuilder = MetaFields.Builder()
+      
+      for pair in metafields! {
+        let val = pair.value
+        if val is String {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! String)
+        } else if val is Bool {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Bool)
+        } else if val is Int {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Int)
+        } else if val is Float {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Float)
+        } else if val is Double {
+          _ = metafieldsBuilder.setField(key: pair.key, value: val as! Double)
+        } else {
+          print("AdaChatService:show: incorrect metafield value=\(val) for key=\(pair.key)")
+        }
+      }
+      
+      adaFramework.setSensitiveMetaFields(builder: metafieldsBuilder)
+    }
+    
+    adaFramework.webViewLoadingErrorCallback = { (error) -> Void in
+      self.channel?.invokeMethod("onError", arguments: error)
+    }
+    adaFramework.eventCallbacks = ["*": { (event) -> Void in
+      self.channel?.invokeMethod("onEvent", arguments: event)
+    }]
+    
+    //        public var zdChatterAuthCallback: (((@escaping (_ token: String) -> Void)) -> Void)?
+    //              zdChatterAuthCallback: (((@escaping (_ token: String) -> Void)) -> Void)? = nil,
+    
+    if mode == "inject" {
+      adaFramework.launchInjectingWebSupport(into: rootViewController!.view)
+    } else if mode == "navigation" {
+      if rootViewController?.navigationController == nil {
+        print("AdaChatService:show: navigationController=nil")
+        return
+      }
+      adaFramework.launchNavWebSupport(from: rootViewController!.navigationController!)
+    } else {
+      if rootViewController == nil {
+        print("AdaChatService:show: rootViewController=nil")
+        return
+      }
+      adaFramework.launchModalWebSupport(from: rootViewController!)
+    }
+    
+    self.adaFramework = adaFramework
+  }
+  
+  func setLanguage(language: String) {
+    if self.adaFramework == nil {
+      print("AdaChatService:setLanguage: adaFramework is not initialized")
+    }
+    
+    debugPrint("AdaChatService:setLanguage: language=\(String(describing: language))")
+    
+    self.adaFramework!.setLanguage(language: language)
+  }
+  
+  func setMetaFields(metafields: [String:Any]) {
+    if self.adaFramework == nil {
+      print("AdaChatService:setMetaFields: adaFramework is not initialized")
+    }
+    
+    debugPrint("AdaChatService:setMetaFields: metafields=\(String(describing: metafields))")
+    
+    let metafieldsBuilder = MetaFields.Builder()
+    
+    for pair in metafields {
+      let val = pair.value
+      if val is String {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! String)
+      } else if val is Bool {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Bool)
+      } else if val is Int {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Int)
+      } else if val is Float {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Float)
+      } else if val is Double {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Double)
+      } else {
+        print("AdaChatService:setMetaFields: incorrect metafield value=\(val) for key=\(pair.key)")
+      }
+    }
+    
+    self.adaFramework!.setMetaFields(builder: metafieldsBuilder)
+  }
+  
+  func setSensitiveMetaFields(metafields: [String:Any]) {
+    if self.adaFramework == nil {
+      print("AdaChatService:setSensitiveMetaFields: adaFramework is not initialized")
+    }
+    
+    debugPrint("AdaChatService:setSensitiveMetaFields: metafields=\(String(describing: metafields))")
+    
+    let metafieldsBuilder = MetaFields.Builder()
+    
+    for pair in metafields {
+      let val = pair.value
+      if val is String {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! String)
+      } else if val is Bool {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Bool)
+      } else if val is Int {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Int)
+      } else if val is Float {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Float)
+      } else if val is Double {
+        _ = metafieldsBuilder.setField(key: pair.key, value: val as! Double)
+      } else {
+        print("AdaChatService:setSensitiveMetaFields: incorrect metafield value=\(val) for key=\(pair.key)")
+      }
+    }
+    
+    self.adaFramework!.setSensitiveMetaFields(builder: metafieldsBuilder)
+  }
+  
+  
+  //
+  //     func loginUser(jwt: String) {
+  //         Zendesk.instance?.loginUser(with: jwt) { result in
+  //             switch result {
+  //             case .success(let user):
+  //                 self.channel?.invokeMethod(ZendeskMessaging.loginSuccess, arguments: ["id": user.id, "externalId": user.externalId])
+  //                 break;
+  //             case .failure(let error):
+  //                 debugPrint("\(self.TAG) - login failure - \(error.localizedDescription)\n")
+  //                 self.channel?.invokeMethod(ZendeskMessaging.loginFailure, arguments: ["error": nil])
+  //                 break;
+  //             }
+  //         }
+  //     }
+  //
+  //     func logoutUser() {
+  //         Zendesk.instance?.logoutUser { result in
+  //             switch result {
+  //             case .success:
+  //                 self.channel?.invokeMethod(ZendeskMessaging.logoutSuccess, arguments: [])
+  //                 break;
+  //             case .failure(let error):
+  //                 debugPrint("\(self.TAG) - logout failure - \(error.localizedDescription)\n")
+  //                 self.channel?.invokeMethod(ZendeskMessaging.logoutFailure, arguments: ["error": nil])
+  //                 break;
+  //             }
+  //         }
+  //     }
+  //     func getUnreadMessageCount() -> Int {
+  //         let count = Zendesk.instance?.messaging?.getUnreadMessageCount()
+  //         return count ?? 0
+  //     }
 }
